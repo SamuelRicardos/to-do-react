93e1abd356bad03f31e2e607525ff43837f45231
SamuelRicardos <samuelric4rdo@gmail.com>
2025-04-23 22:23:02 -0300

    ajustes nos estilos e adicao do scroll

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     useEffect(() => {
         const tasksSalvas = localStorage.getItem("@tasks")
 
-        if(tasksSalvas) {
+        if (tasksSalvas) {
             setTarefas(JSON.parse(tasksSalvas));
         }
     }, [])
@@ -30,7 +30,7 @@ export default function Home() {
     function submitForm(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
-        if(id) {
+        if (id) {
             editTask();
         } else {
             const novaTarefa: Task = {
@@ -45,7 +45,7 @@ export default function Home() {
             setTarefas(novasTarefas);
 
             localStorage.setItem("@tasks", JSON.stringify(novasTarefas))
-    
+
             setTitulo("")
             setDescricao("")
             setData("")
@@ -63,7 +63,7 @@ export default function Home() {
         setId(tarefa.id)
     }
 
-    function editTask(){
+    function editTask() {
         const posicaoArray = tarefas.findIndex((tarefa) => tarefa.id == id);
 
         const copiaTarefas = [...tarefas]
@@ -96,39 +96,39 @@ export default function Home() {
         setTarefas(arrayFiltrado)
     }
 
-    return(
-        <div className="container_home"> 
+    return (
+        <div className="container_home">
             <div className="form">
                 <form onSubmit={submitForm}>
                     <h2>Cadastrar tarefa</h2>
-                    <input 
-                    type="text" 
-                    placeholder="Título"
-                    value={titulo}
-                    onChange={(event) => setTitulo(event.target.value)}
+                    <input
+                        type="text"
+                        placeholder="Título"
+                        value={titulo}
+                        onChange={(event) => setTitulo(event.target.value)}
                     />
 
                     <select
-                    aria-placeholder="Categoria"
-                    value={categoria}
-                    onChange={(event) => setCategoria(event.target.value)}
+                        aria-placeholder="Categoria"
+                        value={categoria}
+                        onChange={(event) => setCategoria(event.target.value)}
                     >
                         <option value=""></option>
                         <option value="Trabalho">Trabalho</option>
                         <option value="Estudo">Estudo</option>
                     </select>
 
-                    <input 
-                    type="date" 
-                    placeholder="Data"
-                    value={data}
-                    onChange={(event) => setData(event.target.value)}
-                     />
+                    <input
+                        type="date"
+                        placeholder="Data"
+                        value={data}
+                        onChange={(event) => setData(event.target.value)}
+                    />
 
-                    <textarea 
-                    placeholder="Descrição"
-                    value={descricao}
-                    onChange={(event) => setDescricao(event.target.value)}
+                    <textarea
+                        placeholder="Descrição"
+                        value={descricao}
+                        onChange={(event) => setDescricao(event.target.value)}
                     ></textarea>
 
                     <button type="submit">Salvar</button>
@@ -139,21 +139,22 @@ export default function Home() {
                 <h2>Lista de tarefas - Total: {tarefas.length}</h2>
 
                 {tarefas.length < 1 && "Nenhuma tarefa cadastrada"}
-
-                <ul>
-                    {tarefas.map((tarefa) => (
-                        <li>
-                            <h4>{tarefa.title} - {tarefa.id}</h4>
-                            <p>{tarefa.category}</p>
-                            <p>{tarefa.date}</p>
-                            <p>{tarefa.description}</p>
-                            <div>
-                            <MdEdit color="green" size={30} onClick={() => preencheEstados(tarefa)} />
-                            <MdDelete color="red" size={30} onClick={() => apagarTarefa(tarefa.id)} />
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                <div className="scrollable_tasks">
+                    <ul>
+                        {tarefas.map((tarefa) => (
+                            <li>
+                                <h4>{tarefa.title}</h4>
+                                <p>{tarefa.category}</p>
+                                <p>{tarefa.date}</p>
+                                <p>{tarefa.description}</p>
+                                <div>
+                                    <MdEdit color="green" size={30} onClick={() => preencheEstados(tarefa)} />
+                                    <MdDelete color="red" size={30} onClick={() => apagarTarefa(tarefa.id)} />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
